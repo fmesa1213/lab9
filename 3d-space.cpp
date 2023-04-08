22b51a88f5facae3fd9f8a5403af7f3d24bb678b
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2023-04-07 22:55:31 -0400

    Update 3d-space.cpp

--- 3d-space.cpp
+++ 3d-space.cpp
@@ -40,9 +40,61 @@ v.z = v.z + (X.z*dt);
 *ppos = v;
 }
 
+Coord3D* createCoord3D(double x, double y, double z)
 
-int main() 
 {
+
+cout<<"\ndynamically allocated memory for structure"<<endl;
+
+Coord3D* ppos = new Coord3D;
+
+ppos->x = x;
+ppos->y = y;
+ppos->z = z;
+return ppos;
+
+}
+void deleteCoord3D(Coord3D *p)
+
+{
+
+cout<<"Deleting the structure"<<endl;
+delete p;
+
+}
+
+void move(Coord3D pp, Coord3D pv, double t)
+
+{
+
+pp->x = (pv->x*t)+pp->x;
+pp->y = (pv->y*t)+pp->y;
+pp->z = (pv->z*t)+pp->z;
+
+}
+
+int main()
+
+{
+double x, y, z;
+
+cout<<"Enter position: ";
+cin>>x>>y>>z;
+
+Coord3D *ppos = createCoord3D(x, y, z);
+
+cout<<"\nEnter Velocity:";
+cin>>x>>y>>z;
+
+Coord3D *pvel = createCoord3D(x, y, z);
+
+move(ppos, pvel, 10.0);
+
+cout<<"Coordinates after 10 seconds: "<<(*ppos).x<<" "<<(*ppos).y<<" "<<(*ppos).z<<endl;
+
+deleteCoord3D(ppos);
+deleteCoord3D(pvel);
+
 Coord3D pointP = {10, 20, 30};
 Coord3D pointQ = {-20, 21, -22};
 
@@ -63,3 +115,6 @@ cout << pos.x << " " << pos.y << " " << pos.z << endl;
 
 return 0;
 }
+
+
+
